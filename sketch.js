c668b9a0edcb0c6b77f23a6d2453e0dcee804d95
tmurugesh <73613956+tmurugesh@users.noreply.github.com>
2021-02-06 20:57:56 -0800

    update sketch

--- sketch.js
+++ sketch.js
@@ -26,25 +26,32 @@ function draw() {
   drawClothes();
  }
 
+// draws a bunch of facial features
 function drawFace(){
 
+	// smile
 	stroke("#d84ba0");
 	strokeWeight(10);
 	noFill();
 	arc(500,680, 180,80, 0.1,3);
 
+	// eyebrows
 	stroke(0);
 	strokeWeight(17);
 	arc(385,510, 160,80, 4.2,6);
 	arc(615,510, 160,80, 3.4,5.2);
 
+	//blush
 	fill("#f458a6");
 	noStroke();
 	ellipse(350, 650, 100, 100);
 	ellipse(650, 650, 100, 100);
 
-	stroke("#a0e81b");
+	//glasses
+	stroke("#ffc907");
+	//noStroke();
 	noFill();
+	//fill("#a0e81b66")
 	strokeWeight(10);
 	rect(370, 570, 170, 150, 20,20,40,40);
 	rect(630, 570, 170, 150, 20,20,40,40);
@@ -53,15 +60,16 @@ function drawFace(){
 	
 	noStroke();
 
+	//nose
 	fill("#d84ba0");
 	ellipse(500, 650, 60, 40);
 
 
-	
 	fill(0);
 	translate(-50,-50);
 	scale(1.1);
 
+	// left eye
 	beginShape();
 	vertex(450,540);
 	vertex(430,520);
@@ -83,7 +91,7 @@ function drawFace(){
 	vertex(427,545);
 	endShape(CLOSE);
 	
-
+	// right eye
 	beginShape();
 	vertex(550,540);
 	vertex(570,520);
@@ -105,18 +113,50 @@ function drawFace(){
 	vertex(573,545);
 	endShape(CLOSE);
 
+	//eye shine
+
+	fill("#ffc907");
+	//ellipse(360,550,25,25);
+	//ellipse(380,570,15,15);
+
+	//ellipse(580,550,25,25);
+	//ellipse(600,570,15,15);
+	noFill();
+	strokeWeight(2);
+	stroke("#ffc907");
+	ellipse(610,577,45,15);
+	ellipse(390,577,45,15);
 
 
-	fill("#baf704");
-	ellipse(360,550,25,25);
-	ellipse(380,570,15,15);
+	fill("#ffc907");
+	noStroke();
+	push();
+ 	translate(width /2.7, height /1.85);
+  	rotate(frameCount / -100.0);
+  	star(0, 0, 9, 17, 4);
+  	pop();
 
-	ellipse(580,550,25,25);
-	ellipse(600,570,15,15);
+  	push();
+ 	translate(width /2.55, height /1.75);
+  	rotate(frameCount / -100.0);
+  	star(0, 0, 5, 12, 7);
+  	pop();
 
+  	push();
+ 	translate(width /1.7, height /1.85);
+  	rotate(frameCount / -100.0);
+  	star(0, 0, 9, 17, 4);
+  	pop();
+
+  	push();
+ 	translate(width /1.64, height /1.75);
+  	rotate(frameCount / -100.0);
+  	star(0, 0, 5, 12, 7);
+  	pop();
 
 }
 
+// draws the head
 function drawHead(){
 	noStroke();
 	fill("#f477d1");
@@ -134,24 +174,28 @@ function drawHead(){
 	vertex(770,550);
 	vertex(700,686)
 	vertex(630, 745);
-
 	vertex(560, 786);
 	endShape();
 }
 
 //draws ears and neck shapes
 function drawEar(){
+	// ear
 	noStroke();
 	fill("#f477d1");
 	ellipse(220, 545, 140,190);
 	ellipse(780, 545, 150,190);
 
+	// neck
 	fill("#d84ba0");
 	rect(500, 740, 150, 180);
 }
 
+// draws bangs
 function drawBangs(){
 	noStroke();
+
+	// left side bangs
 	fill("#f2299e");
 	beginShape();
 	vertex(500,180);
@@ -171,6 +215,7 @@ function drawBangs(){
 	vertex(450,170);
 	endShape(CLOSE);
 
+	//right side bangs
 	fill("#853cc1");
 	beginShape();
 	vertex(500,180);
@@ -189,8 +234,14 @@ function drawBangs(){
 	vertex(620,190);
 	vertex(550,170);
 	endShape(CLOSE);
+
+	stroke("#f477d1");
+	strokeWeight(10);
+	noFill();
+	arc(490,230, 380,100, 0.1,3);
 }
 
+// draws the back of the hair
 function drawBackHair(){
 	noStroke();
 	fill(0);
@@ -231,11 +282,14 @@ function drawBackHair(){
 	endShape(CLOSE);
 }
 
+// draws clothing
 function drawClothes(){
 
-	fill("#baf704");
+	// turtleneck 
+	fill("#ffc907");
     rect(500, 870, 190, 180);
 
+    // sweater
 	fill(0);
 	//arc(500,1040,800,487,PI, TWO_PI);
 	beginShape();
@@ -253,6 +307,20 @@ function drawClothes(){
 
     }
 
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
 // shows x and y value at mouse point, toggles off and on by space bar
 function drawDebugInfo(){
 	fill(255);

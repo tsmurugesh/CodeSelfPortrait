9957cc9cc4d6cc5bb92f955b7830c113a834ed6f
tmurugesh <73613956+tmurugesh@users.noreply.github.com>
2021-02-06 16:31:34 -0800

    update sketch

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
+var gDebugMode = false;
 
 // Setup code goes here
 function setup() {
   createCanvas(1000, 1000);
+
+  textSize(20);
+  textAlign(LEFT);
  }
 
 
 // Draw code goes here
 function draw() {
   background("blue");
+  if (gDebugMode == true){
+  	drawDebugInfo();
+  }
+}
+
+function drawDebugInfo(){
+	fill(255);
+	text("x: "+ mouseX + " y: " + mouseY, 2, height-2);
+}
+
+function keyTyped(){
+	if (key === " "){
+		gDebugMode = !gDebugMode;
+	}
 }

7cbdeab9a74662aefbee95a399a8abdf9d6ba268
tmurugesh <73613956+tmurugesh@users.noreply.github.com>
2021-02-06 18:55:41 -0800

    update sketch

--- sketch.js
+++ sketch.js
@@ -13,13 +13,78 @@ function setup() {
 // Draw code goes here
 function draw() {
   background("blue");
+
+    if (gDebugMode == true){
+  	drawDebugInfo();
+  }
   drawBackHair();
   drawEar();
   drawHead();
   drawBangs();
-  if (gDebugMode == true){
-  	drawDebugInfo();
-  }
+  drawFace();
+  drawClothes();
+ }
+
+function drawFace(){
+
+	stroke(0);
+	strokeWeight(10);
+	noFill();
+	arc(500,660, 180,80, 0,PI);
+
+	strokeWeight(17);
+	arc(385,510, 160,80, 4.2,6);
+	arc(615,510, 160,80, 3.4,5.2);
+	
+	noStroke();
+	fill(0);
+	translate(-50,-50);
+	scale(1.1);
+
+	beginShape();
+	vertex(450,540);
+	vertex(430,520);
+	vertex(390,500); 
+	vertex(360,505);
+	vertex(330,520);
+	vertex(305,530); //wing//
+	vertex(320,550); 
+	vertex(350,540); //pupil turn// 
+	vertex(350,570);
+	vertex(360,590);
+	vertex(355,605);
+	vertex(380,595);
+	vertex(390,605);
+	vertex(400,595);
+	vertex(425,605);
+	vertex(425,595);
+	vertex(430,570);
+	vertex(427,545);
+	endShape(CLOSE);
+	
+
+	beginShape();
+	vertex(550,540);
+	vertex(570,520);
+	vertex(610,500); 
+	vertex(640,505);
+	vertex(670,520);
+	vertex(695,530); //wing//
+	vertex(680,550); 
+	vertex(650,540); //pupil turn// 
+	vertex(650,570);
+	vertex(640,590);
+	vertex(645,605);
+	vertex(620,595);
+	vertex(610,605);
+	vertex(600,595);
+	vertex(575,605);
+	vertex(575,595);
+	vertex(570,570);
+	vertex(573,545);
+	endShape(CLOSE);
+
+
 }
 
 function drawHead(){
@@ -28,6 +93,7 @@ function drawHead(){
 	beginShape();
 	vertex(500,800); //bottom, clockwise//
 	vertex(440, 786);
+	vertex(360, 740);
 	vertex(300,686);
 	vertex(230,580);
 	vertex(220,600); //left//
@@ -37,6 +103,8 @@ function drawHead(){
 	vertex(780,600); //right//
 	vertex(770,580);
 	vertex(700,686)
+	vertex(630, 745);
+
 	vertex(560, 786);
 	endShape();
 }
@@ -45,8 +113,9 @@ function drawHead(){
 function drawEar(){
 	noStroke();
 	fill("#f477d1");
-	ellipse(200, 550, 170,200);
-	ellipse(800, 550, 170,200);
+	ellipse(200, 540, 170,190);
+	ellipse(800, 540, 170,190);
+
 	fill("#ea56b1");
 	rect(425, 740, 150, 180);
 }
@@ -56,12 +125,13 @@ function drawBangs(){
 	fill("#f2299e");
 	beginShape();
 	vertex(500,180);
-	vertex(500,270);
-	vertex(440,340);
-	vertex(410,380);
+	vertex(500,320);
+	vertex(440,390);
+	vertex(430,460);
 	//vertex(390,330);//
-	vertex(340,410);
-	vertex(300,430);
+	vertex(380,400); //up point
+	vertex(340,480);
+	vertex(300,480);
 	vertex(280,530);
 	vertex(250,470);
 	vertex(210,450);
@@ -74,18 +144,19 @@ function drawBangs(){
 	fill("#853cc1");
 	beginShape();
 	vertex(500,180);
-	vertex(500,270);
-	vertex(560,340);
-	vertex(570,380);
+	vertex(500,320);
+	vertex(560,390);
+	vertex(570,460);
 	//vertex(390,330);//
-	vertex(640,410);
-	vertex(680,430);
-	vertex(700,530);
-	vertex(730,470);
-	vertex(770,450);
-	vertex(750,350);
-	vertex(700,250);
-	vertex(630,190);
+	vertex(620,400); //up point
+	vertex(660,480);
+	vertex(700,480);
+	vertex(720,530);
+	vertex(750,470);
+	vertex(790,450);
+	vertex(770,350);
+	vertex(690,250);
+	vertex(620,190);
 	vertex(550,170);
 	endShape(CLOSE);
 }
@@ -93,7 +164,7 @@ function drawBangs(){
 function drawBackHair(){
 	noStroke();
 	fill(0);
-	let backLeft = beginShape();
+	beginShape();
 	vertex(470,100);
 	vertex(400,130);
 	vertex(250,210);
@@ -111,11 +182,46 @@ function drawBackHair(){
 	vertex(320,800);
 	vertex(360,830);
 	vertex(500,850); //center bottom//
-	//vertex(640,830);
+	vertex(640,830);
+	vertex(680,800);
+	vertex(730,740);
+	vertex(760,830);
+	vertex(785,790);
+	vertex(790,730);
+	vertex(780,670);
+	vertex(840,660);
+	vertex(920,640);
+	vertex(950,600);
+	vertex(880,550);
+	vertex(830,420);
+	vertex(820,300);
+	vertex(760,210);
+	vertex(610,130);
+	vertex(530,100);
 	endShape(CLOSE);
-
 }
 
+function drawClothes(){
+
+	fill("cyan");
+    rect(405, 778, 190, 180);
+
+	fill(39);
+	//arc(500,1040,800,487,PI, TWO_PI);
+	beginShape();
+	vertex(80,1000);
+	vertex(200,880);
+	vertex(300,800);
+	vertex(700,800);
+	vertex(920,1000);
+	beginContour();
+	vertex(400,800);
+	vertex(500,950);
+	vertex(600,800);
+	endContour(CLOSE);
+	endShape(CLOSE);
+
+    }
 
 // shows x and y value at mouse point, toggles off and on by space bar
 function drawDebugInfo(){
